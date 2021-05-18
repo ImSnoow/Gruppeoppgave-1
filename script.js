9c671312865b157842f8a7e6b289946d233d51d5
MortenWaage <84008835+MortenWaage@users.noreply.github.com>
2021-05-18 17:13:33 +0200

    Alert OnClick

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ function addDivs() {
     {
         var div = document.createElement('div');
         div.className = 'box';
+        div.addEventListener("click", function()
+        {
+            clickScript(this.id);
+        });
         div.id = 'div' + parseInt(n+1);
         container.appendChild(div); 
         n++;
     }
+}
+
+function clickScript(id)
+{   
+    alert("Klikket i rute nr: " + id);
 }
