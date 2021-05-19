f73219bfbde62e7d415b7d4c8cf721c63e2ba57e
MortenWaage <84008835+MortenWaage@users.noreply.github.com>
2021-05-19 09:28:38 +0200

    For Loop
    Byttet While statement til For loop

--- script.js
+++ script.js
@@ -2,14 +2,12 @@
 function addDivs() {
     container = document.getElementById("container");
         
-    n = 0;
     size = 9;
-    while (n < size)
+    for (n = 0; n < size; n++)
     {
         var div = document.createElement('div');
         div.className = 'box';
         div.id = 'div' + parseInt(n+1);
         container.appendChild(div); 
-        n++;
     }
 }
